8db85843aaab219466c7c055234c8d9b553938a5
the-eduek <the-eduek@users.noreply.github.com>
2026-03-22 11:42:14 +0000

    deploy: 39d8fff36bfacfe69319020b0d93c535f9bec41c

---
+++ assets/index-cb1cf503.js
@@ -0,0 +1,13 @@
+(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function l(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=l(e);fetch(e.href,t)}})();const g=`
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M12 3v1m0 16v1m-8-9H3m3.314-5.686L5.5 5.5m12.186.814L18.5 5.5M6.314 17.69l-.814.81m12.186-.81.814.81M21 12h-1m-4 0a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"
+      stroke="#bbb" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+    />
+  </svg>`,h=`
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M3.32 11.684a9 9 0 0 0 17.357 3.348A9 9 0 0 1 8.32 6.683c0-1.18.23-2.32.644-3.353a9.003 9.003 0 0 0-5.645 8.354Z"
+      fill="#666" stroke="#666" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+    />
+  </svg>`,d=document.querySelector("[data-btn-theme]"),c=document.querySelector("body");function p(){const n=localStorage.getItem("theme");return n==="light"||n==="dark"?n:window.matchMedia("(prefers-color-scheme: light)").matches?"light":"dark"}function m(n){n==="light"?(c.classList.add("light"),d.innerHTML=h):(c.classList.remove("light"),d.innerHTML=g)}m(p());requestAnimationFrame(()=>{requestAnimationFrame(()=>{c.style.transition="background 250ms ease-out, color 250ms ease-out"})});d.addEventListener("click",()=>{const n=c.classList.contains("light")?"dark":"light";localStorage.setItem("theme",n),m(n)});window.matchMedia("(prefers-color-scheme: light)").addEventListener("change",n=>{localStorage.getItem("theme")||m(n.matches?"light":"dark")});function y(n,i,l,s){function e(){const t=new Date,o=t.getHours(),u=t.getFullYear(),a=t.getMinutes();function f(r){return r===0?"12":(r>12&&(r=r-12),r<10&&r<12?`0${r}`:`${r}`)}n.innerHTML=f(o),i.innerHTML=a<10?`0${a}`:`${a}`,l.innerHTML=o<12?"AM":"PM",s.innerHTML=u.toString()}e(),setInterval(e,1e3)}const v=document.querySelector("[data-hour]"),w=document.querySelector("[data-minute]"),L=document.querySelector("[data-year]"),M=document.querySelector("[data-meridiem]");y(v,w,M,L);
